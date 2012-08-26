74cba38c90709c3729a85aaa23a1abcb11217798
ragge <ragge>
2012-08-26 17:07:31 +0000

    fix STCALL and UMUL code generation problems.

--- arch/vax/table.c
+++ arch/vax/table.c
@@ -1,4 +1,4 @@
-/*	$Id: table.c,v 1.22 2012/08/07 06:24:57 ragge Exp $	*/
+/*	$Id: table.c,v 1.23 2012/08/26 17:07:31 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -326,7 +326,11 @@ struct optab  table[] = {
 		NAREG|NASL,	RESC1, /* should be register 0 */
 		"	calls	ZC,CL\n", },
 
-
+{ STCALL,	INAREG,
+	SAREG,	TANY,
+	SANY,	TAREG,
+		NAREG|NASL,	RESC1,	/* should be 0 */
+		"	calls	ZC,(AL)\n", },
 
 /*
  * Function arguments
@@ -491,8 +495,8 @@ struct optab  table[] = {
 		"	HELP HELP HELP\n", },
 
 { UMUL, INBREG,
-	SANY,	TPOINT|TLL,
-	SOREG,	TPOINT|TLL,
+	SANY,	TPOINT,
+	SOREG,	TBREG,
 		NBREG|NBSL,	RESC1,
 		"	movq AL,A1\n", },
 
